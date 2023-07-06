d8b8842867fc29a270ad8cf202c6981501aa0b9d
yxcheng <yixincheng@pateo.com.cn>
2023-07-06 22:11:49 +0800

    refactor(update): code

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,11 @@
 function Message() {
     // JSX: JavaScript XML
-    return <h1>Hello World</h1>;
+    const name = "CCC"
+    if (name) {
+        return <h1>Hello World {name}</h1>;
+    } else {
+        return <h1>Hello World</h1>;
+    }
 }
 
 export default Message;
