e8b150fa4f38cda495d2d2062bfa9ed8d2686110
yxcheng <yixincheng@pateo.com.cn>
2023-07-07 12:36:49 +0800

    refactor(code): update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import ListGroup from "./components/ListGroup";
 function App() {
     // return <div><Message/></div>
 
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    }
+
     let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-    return <div><ListGroup items={items} heading="Cities"/></div>;
+
+    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
 }
 
 export default App;
