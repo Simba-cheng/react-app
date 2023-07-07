a555a65bb4802dfa6be44c61fcc7b085941181ef
yxcheng <yixincheng@pateo.com.cn>
2023-07-07 09:34:14 +0800

    refactor(code): update

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+import {Fragment} from "react";
+
+function ListGroup() {
+    let items = [
+        'New York', 'San Francisco', 'Tokyo', 'London', 'Paris'
+    ];
+    items = [];
+
+    const getMessage = () => {
+        return items.length === 0 ? <p>No item found</p> : null;
+    }
+
+    return (
+        <>
+            <h1>List Group</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
